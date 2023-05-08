2e2c6670dbe49fac1560fdc339d2b74b5cba9dcc
John Estabrook <jestabro@vyos.io>
2023-05-07 20:38:33 -0500

    test relative defaults, merge

--- python/vyos/xml_def.py
+++ python/vyos/xml_def.py
@@ -1,6 +1,6 @@
 
 from typing import Union
-from vyos.xml_cache_generate import node_data_fields as data_fields
+from vyos.util import get_sub_dict
 
 class Xml:
     def __init__(self):
@@ -11,12 +11,15 @@ class Xml:
 
         self.defs = definition
 
+    def define(self, ref: dict):
+        self.defs = ref
+
     def _get_ref_node_data(self, node: dict, data: str) -> Union[bool, str]:
-        if data_fields is not None and data not in data_fields:
-            raise ValueError("data field not available")
         res = node.get('node_data', {})
         if not res:
-            raise ValueError("non-existent node")
+            raise ValueError("non-existent node data")
+        if data not in res:
+            raise ValueError("non-existent data field")
 
         return res.get(data)
 
@@ -100,8 +103,7 @@ class Xml:
                 if k == 'node_data':
                     continue
                 pos = self.get_defaults(path + [k])
-                if pos:
-                    res |= pos
+                res |= pos
         if res:
             if get_first_key:
                 if not isinstance(res, dict):
@@ -111,9 +113,43 @@ class Xml:
 
         return {}
 
-    def fill_defaults(self, rpath: list, conf: dict) -> dict:
-        pass
+    def relative_defaults(self, path: list, conf: dict = None, get_first_key=False) -> dict:
+        if conf is None:
+            conf = {}
+        res = {}
+        d = self._get_ref_path(path)
+        if self._is_leaf_node(d):
+            default_value = self._get_default_value(d)
+            if default_value is not None:
+                res = default_value
+                if self._is_multi_node(d) and not isinstance(res, list):
+                    res = [res]
+        elif self.is_tag(path):
+            for k in list(conf):
+                pos = self.relative_defaults(path + [k], conf[k])
+                res |= pos
+        else:
+            for k in list(d):
+                if k == 'node_data':
+                    continue
+                pos = self.relative_defaults(path + [k], conf.get(k, {}))
+                res |= pos
+        if res:
+            if get_first_key:
+                if not isinstance(res, dict):
+                    raise TypeError("Cannot get_first_key as data under node is not of type dict")
+                return res
+            return {path[-1]: res}
 
-    @staticmethod
-    def generate_cache():
-        pass
+        return {}
+
+    def merge_defaults(self, path: list, conf: dict) -> dict:
+        if path[-1] in list(conf):
+            conf = conf[path[-1]]
+            if not isinstance(conf, dict):
+                raise ValueError('conf at path is not of type dict')
+            first = False
+        else:
+            first = True
+
+        return self.relative_defaults(path, conf=conf, get_first_key=first)
