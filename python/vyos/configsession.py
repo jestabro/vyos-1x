e94203ade3bfb69e1d0c1539d33ed88da448633e
John Estabrook <jestabro@vyos.io>
2025-05-23 10:40:40 -0500

    T7488: add utility for automatic rollback of section on apply stage err

--- python/vyos/configsession.py
+++ python/vyos/configsession.py
@@ -141,7 +141,7 @@ class ConfigSession(object):
     The write API of VyOS.
     """
 
-    def __init__(self, session_id, app=APP):
+    def __init__(self, session_id, app=APP, shared=False):
         """
          Creates a new config session.
 
@@ -182,7 +182,11 @@ class ConfigSession(object):
         else:
             self._vyconf_session = None
 
+        self.shared = shared
+
     def __del__(self):
+        if self.shared:
+            return
         if self._vyconf_session is None:
             try:
                 output = (
