e2a7fd4204341fed28c0a004122fa200c103aa5a
John Estabrook <jestabro@vyos.io>
2026-08-19 16:49:24 -0500

    T9231: add wrappers for reference_tree utils

--- python/vyos/referencetree.py
+++ python/vyos/referencetree.py
@@ -26,7 +26,7 @@ class ReferenceTreeError(Exception):
 
 
 class ReferenceTree:
-    # pylint: disable=too-many-instance-attributes,raise-missing-from
+    # pylint: disable=too-many-instance-attributes,raise-missing-from,import-outside-toplevel
     def __init__(self, cache_file=reference_tree_cache, libpath=LIBPATH):
         self.__pointer = None
         self.__lib = cdll.LoadLibrary(libpath)
@@ -53,6 +53,30 @@ class ReferenceTree:
         self.__equal = self.__lib.equal
         self.__equal.argtypes = [c_void_p, c_void_p]
         self.__equal.restype = c_bool
+
+        self.__get_owner = self.__lib.get_owner
+        self.__get_owner.argtypes = [c_void_p, c_char_p]
+        self.__get_owner.restype = c_char_p
+
+        self.__get_multi_nodes = self.__lib.get_multi_nodes
+        self.__get_multi_nodes.argtypes = [c_void_p, c_char_p]
+        self.__get_multi_nodes.restype = c_char_p
+
+        self.__get_tag_nodes = self.__lib.get_tag_nodes
+        self.__get_tag_nodes.argtypes = [c_void_p, c_char_p]
+        self.__get_tag_nodes.restype = c_char_p
+
+        self.__get_nodes_of_kind = self.__lib.get_nodes_of_kind
+        self.__get_nodes_of_kind.argtypes = [c_void_p, c_char_p, c_char_p]
+        self.__get_nodes_of_kind.restype = c_char_p
+
+        self.__get_rdeps_of_kind = self.__lib.get_rdeps_of_kind
+        self.__get_rdeps_of_kind.argtypes = [c_void_p, c_char_p, c_char_p]
+        self.__get_rdeps_of_kind.restype = c_char_p
+
+        self.__get_rdeps_of_kind_data = self.__lib.get_rdeps_of_kind_data
+        self.__get_rdeps_of_kind_data.argtypes = [c_void_p, c_char_p, c_char_p]
+        self.__get_rdeps_of_kind_data.restype = c_char_p
 
         try:
             cache_string = Path(cache_file).read_bytes()
@@ -85,3 +109,49 @@ class ReferenceTree:
 
     def to_json(self):
         return self.__to_json(self.__pointer).decode()
+
+    def get_owner(self, path):
+        return self.__get_owner(self.__pointer, path.encode()).decode()
+
+    def get_multi_nodes(self, tag_value_placeholder='', as_tuple=False):
+        import json
+
+        res = self.__get_multi_nodes(
+            self.__pointer, tag_value_placeholder.encode()
+        ).decode()
+        sort = sorted(json.loads(res))
+        return list(map(tuple, sort)) if as_tuple else sort
+
+    def get_tag_nodes(self, tag_value_placeholder='', as_tuple=False):
+        import json
+
+        res = self.__get_tag_nodes(
+            self.__pointer, tag_value_placeholder.encode()
+        ).decode()
+        sort = sorted(json.loads(res))
+        return list(map(tuple, sort)) if as_tuple else sort
+
+    def get_nodes_of_kind(self, kind, tag_value_placeholder=''):
+        import json
+
+        res = self.__get_nodes_of_kind(
+            self.__pointer, kind.encode(), tag_value_placeholder.encode()
+        ).decode()
+        return sorted(json.loads(res))
+
+    def get_rdeps_of_kind(self, kind, tag_value_placeholder=''):
+        import json
+
+        res = self.__get_rdeps_of_kind(
+            self.__pointer, kind.encode(), tag_value_placeholder.encode()
+        ).decode()
+        return sorted(json.loads(res))
+
+    def get_rdeps_of_kind_data(self, kind, tag_value_placeholder='', as_tuple=True):
+        import json
+
+        res = self.__get_rdeps_of_kind_data(
+            self.__pointer, kind.encode(), tag_value_placeholder.encode()
+        ).decode()
+        sort = sorted(json.loads(res))
+        return list(map(tuple, sort)) if as_tuple else sort
